351ef2930b11b7296ef3e8279b71a5dc9a30a2b2
Peter Millspaugh <peterdgmillspaugh@gmail.com>
2021-06-12 16:21:24 -0400

    add link to create-post from feed

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Feed = () => {
   return (
     <main>
       <h1>Feed page</h1>
       <p>Feed stuff will go here</p>
+      <Link to='/create-post'>
+        <button>Create post</button>
+      </Link>
     </main>
   );
 };
